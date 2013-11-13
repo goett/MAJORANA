fe9daa00321ac3e4361512482dee82eef939a15e
Johnny <goettj@gmail.com>
2013-11-13 13:15:07 -0700

    Ported AutoCal data into a more convenient struct object.

--- Analyze/COPPIs/AutoCal.C
+++ Analyze/COPPIs/AutoCal.C
@@ -1,11 +1,41 @@
+#include<vector>
+#include<algorithm>
+#include "TH1.h"
+#include "TChain.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include "TSpectrum.h"
+#include "TFitResult.h"
+//#include "RooFit.h"
 
+struct calData{
+    Float_t adc; //TSpectrum guess
+    Float_t fadc; //Fitted peak position
+    Float_t efadc; //error in fitted peak position    
+};
 
+bool CompareByadc(const calData& a, const calData& b){
+	return a.adc < b.adc;
+}
+
+//TGraphErrors* ExtractCalCurve(vector<Float_t>& FitPositions, vector<Float_t>& Energies)
+void ExtractCalCurve(vector<Float_t>& FitPositions, vector<Float_t>& Energies)
 {
-    #include<vector>
-    #include<algorithm>
+    //Get highest energy peak
+    
+    
+    
+    //Extrapolate coefficient
+    
+    //
+    return; 
+}
 
-    gSystem->Load("libRooFit");
-    using namespace RooFit;
+void Play()
+{
+
+    //gSystem->Load("libRooFit");
+    //using namespace RooFit;
 
     TChain* S = new TChain("COPPI_Tree");
     //Look at COPPI calibration data from Nov 2013
@@ -21,9 +51,9 @@
    c2->Divide(1,2);
     
    // --- Find Peaks in Spectrum ---
-   c1.cd(1);
+   c1->cd(1);
    S->Draw("SISenergy>>hc146","channel==146");
-   c1.cd(2);
+   c1->cd(2);
    S->Draw("SISenergy>>hc147","channel==147");
 
       // we calibrate with Th and Zr so let's build TSpectrum and look for those peaks
@@ -39,82 +69,75 @@
    //Use a gaussian to fit each of the peaks.
    TF1* tgaus = new TF1("tgaus","gaus",0,900E3);
    
-   c1.cd(1);
+   c1->cd(1);
    TSpectrum *s = new TSpectrum(12);
    Int_t nfound = s->Search(hc146,3,"",0.05);
-   vector<Float_t> x146; //TSpectrum guess
-   vector<Float_t> xf146; //Fit result
-   vector<Float_t> y146; //TSpectrum guess
-   vector<Float_t> yf146; //Fit result
+   vector<calData> x146; //TSpectrum guess
    printf("Found %d candidate peaks to fit in channel 146 spectrum:\n",nfound);
    TH1 *hb = s->Background(hc146,20,"same");
-   TH1D* hc146bf = hc146->Clone("hc146bf");
+   TH1D* hc146bf = (TH1D*)hc146->Clone("hc146bf");
    hc146bf->Add(hb,-1);
    if (hb) c1->Update();
    c2->cd(1);
    hc146bf->Draw();;
    Float_t *xpeaks = s->GetPositionX();
-   for (i = 0; i < nfound; i++) {
+   calData d;
+   for (int i = 0; i < nfound; i++) {
       //printf("%f : %f \n",s->GetPositionX()[i],s->GetPositionY()[i]);
-      x146.push_back(s->GetPositionX()[i]);
+      d.adc=s->GetPositionX()[i];
+      x146.push_back(d);
    }
-   sort(x146.begin(),x146.end());
-   for(std::vector<Float_t>::iterator it=x146.begin(); it!=x146.end(); ++it)
+   sort(x146.begin(),x146.end(),CompareByadc);
+   for(std::vector<calData>::iterator it=x146.begin(); it!=x146.end(); ++it)
    {
-      tgaus->SetParameter(1,*it);
-      TFitResultPtr r = hc146bf->Fit(tgaus,"S+","",*it-0.02*(*it),*it+0.02*(*it));
-      xf146.push_back(r->Parameter(1));
-      yf146.push_back(r->ParError(1));
+      tgaus->SetParameter(1,(*it).adc);
+      TFitResultPtr r = hc146bf->Fit(tgaus,"SQ+","",(*it).adc-0.02*((*it).adc),(*it).adc+0.02*((*it).adc));
+      (*it).fadc=r->Parameter(1);
+      (*it).efadc=r->ParError(1);
    }
-   cout << " Ts X \t\t Fit X \t\t err(x) " << endl;
-   for(i = 0; i < nfound; i++)
+   cout << " Ts X \t\t Fit X \t\t\t err(x) " << endl;
+   for(int i = 0; i < nfound; i++)
    {
-      printf("%f \t %f \t +/- \t %f \n",x146[i],xf146[i],yf146[i]);
+      printf("%f \t %f \t +/- \t %f \n",x146[i].adc,x146[i].fadc,x146[i].efadc);
    }
    
-
-   c1.cd(2);
-   Int_t nfound = s->Search(hc147,3,"",0.05);
-   vector<Float_t> x147; //TSpectrum guess
-   vector<Float_t> xf147; //Fit result
-   vector<Float_t> y147; //TSpectrum guess
-   vector<Float_t> yf147; //Fit result
+   c1->cd(2);
+   nfound = s->Search(hc147,3,"",0.05);
+   vector<calData> x147; //TSpectrum guess
    printf("Found %d candidate peaks to fit in channel 147 spectrum:\n",nfound);
-   TH1 *hb2 = s->Background(hc147,20,"same");
-   TH1D* hc147bf = hc147->Clone("hc147bf");
-   hc147bf->Add(hb2,-1);
-   if (hb2) c2->Update();
+   TH1 *hb147 = s->Background(hc147,20,"same");
+   TH1D* hc147bf = (TH1D*)hc147->Clone("hc147bf");
+   hc147bf->Add(hb147,-1);
+   if (hb147) c1->Update();
    c2->cd(2);
    hc147bf->Draw();;
-   Float_t *xpeaks = s->GetPositionX();
-   for (i = 0; i < nfound; i++) {
+   xpeaks = s->GetPositionX();
+   for (int i = 0; i < nfound; i++) {
       //printf("%f : %f \n",s->GetPositionX()[i],s->GetPositionY()[i]);
-      x147.push_back(s->GetPositionX()[i]);
+      d.adc=s->GetPositionX()[i];
+      x147.push_back(d);
    }
-   sort(x147.begin(),x147.end());
-   for(std::vector<Float_t>::iterator it=x147.begin(); it!=x147.end(); ++it)
+   sort(x147.begin(),x147.end(),CompareByadc);
+   for(std::vector<calData>::iterator it=x147.begin(); it!=x147.end(); ++it)
    {
-      tgaus->SetParameter(1,*it);
-      TFitResultPtr r = hc147bf->Fit(tgaus,"S+","",*it-0.02*(*it),*it+0.02*(*it));
-      xf147.push_back(r->Parameter(1));
-      yf147.push_back(r->ParError(1));
+      tgaus->SetParameter(1,(*it).adc);
+      TFitResultPtr r = hc147bf->Fit(tgaus,"SQ+","",(*it).adc-0.02*((*it).adc),(*it).adc+0.02*((*it).adc));
+      (*it).fadc=r->Parameter(1);
+      (*it).efadc=r->ParError(1);
    }
-   cout << " Ts X \t\t Fit X \t\t err(x) " << endl;
-   for(i = 0; i < nfound; i++)
+   cout << " Ts X \t\t Fit X \t\t\t err(x) " << endl;
+   for(int i = 0; i < nfound; i++)
    {
-      printf("%f \t %f \t +/- \t %f \n",x147[i],xf147[i],yf147[i]);
+      printf("%f \t %f \t +/- \t %f \n",x147[i].adc,x147[i].fadc,x147[i].efadc);
    }
-   
 
-   // --- Extract centroids and estimate backgrounds
-
-   // --- Estimate Intensity
+   // --- Estimate Intensities
 
    // --- Fit centroids of peaks with linear function
 
-   // --- Plot residuals
-
    // --- Build Calibration Curve
+   
+   // --- Plot residuals
 
    // --- Parameters ---
    //RooRealVar adc("adc","SIS bits",360E3,410E3);
